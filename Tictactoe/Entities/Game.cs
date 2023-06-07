bdfd9502894d1add0b2854304657f81fad6f4a8a
Igor Adam <emyhrvaremreis@gmail.com>
2023-06-07 15:43:12 +0200

    Database init

---
+++ Tictactoe/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Tictactoe.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        
+        public int PlayerXid { get; set; }
+        public int PlayerYid { get; set; }
+        public VariantType board { get; set; }
+        public int winner { get; set; }
+        public DateTime date { get; set; }
+        
+    }
+}
---
+++ Tictactoe/Entities/GameDBcontext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Tictactoe.Entities
+
+{
+
+    public class GameDBcontext : DbContext
+    {
+       
+        private string connectionString = "Server=tictactoe-db.celeubkghsq8.eu-north-1.rds.amazonaws.com;Database=tictactoe-db;User ID=admin;Password=;";
+
+
+
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>()
+                .Property(r => r.Id)
+                .IsRequired()
+                .HasMaxLength(25);
+
+
+        }
+
+
+        
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+
+
+
+}
